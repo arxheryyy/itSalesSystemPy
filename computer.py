905bd9e6dbc1591ad98b1c9d9b392ffcb778188f
Archer Chua <arxherchua@gmail.com>
2020-08-19 00:03:06 +0800

    Day 4 100daysofcode

---
+++ computer.py
@@ -0,0 +1,7 @@
+class computer:
+    def __init__(self, computerID, memory, storage, clockspeed):
+
+        self.computerID = computerID
+        self.memory = memory
+        self.storage = storage
+        self.clockspeed = clockspeed
---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello! This is my IT sales system")
+print("1. View all sales")
+print("2. Add desktop sale")
+print("3. Add laptop sale")
+
+userInput = input()
+
+if userInput == 1:
+    print("")
+elif userInput == 2:
+
+elif userInput == 3:
+
+else:
