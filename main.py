71c0eddad1d75ad390c4334f92a17af0082e6bc1
Archer Chua <arxherchua@gmail.com>
2020-08-24 00:39:27 +0800

    Day 9 of 100daysofcode

--- main.py
+++ main.py
@@ -13,68 +13,188 @@ laptops = [laptop0, laptop1]
 desktops = [desktop0, desktop1]
 top = Tk()
 top.geometry("200x200")
+
+
 def buttonTest():
     print("works")
+
+
 def showsales():
     window = Toplevel(top)
     window.title("show sales")
     window.geometry("200x200")
-    Label(window, text = "show sales").pack()
+    Label(window, text="show sales").pack()
     y = 0
     for x in laptops:
         Label(window, text="ComputerID").pack()
-        Label(window, text = laptops[y].computerID).pack()
+        Label(window, text=laptops[y].computerID).pack()
         Label(window, text="Memory").pack()
-        Label(window, text= laptops[y].memory).pack()
+        Label(window, text=laptops[y].memory).pack()
         Label(window, text="Storage").pack()
-        Label(window, text= laptops[y].storage).pack()
+        Label(window, text=laptops[y].storage).pack()
         Label(window, text="Clock Speed").pack()
-        Label(window, text= laptops[y].clockspeed).pack()
+        Label(window, text=laptops[y].clockspeed).pack()
         Label(window, text="Weight").pack()
-        Label(window, text= laptops[y].weight).pack()
-        Label(window, text="").pack(padx = 10, pady = 10)
+        Label(window, text=laptops[y].weight).pack()
+        Label(window, text="").pack(padx=10, pady=10)
         y = y+1
 
     z = 0
     for x in desktops:
         Label(window, text="ComputerID").pack()
-        Label(window, text = desktops[z].computerID).pack()
+        Label(window, text=desktops[z].computerID).pack()
         Label(window, text="Memory").pack()
-        Label(window, text= desktops[z].memory).pack()
+        Label(window, text=desktops[z].memory).pack()
         Label(window, text="Storage").pack()
-        Label(window, text= desktops[z].storage).pack()
+        Label(window, text=desktops[z].storage).pack()
         Label(window, text="Clock Speed").pack()
-        Label(window, text= desktops[z].clockspeed).pack()
+        Label(window, text=desktops[z].clockspeed).pack()
         Label(window, text="Monitor").pack()
-        Label(window, text= desktops[z].monitor).pack()
-        Label(window, text="").pack(padx = 10, pady = 10)
-        z = z+ 1
-    
-def store(computerID,memory,storage,clockspeed,weight,monitor):
-    computerID.get()
+        Label(window, text=desktops[z].monitor).pack()
+        Label(window, text="").pack(padx=10, pady=10)
+        z = z + 1
+
 
 def addD():
     window = Toplevel(top)
     window.title("Add desktop sale")
     window.geometry("200x200")
-    Label(window, text = "Add desktop sale").pack()
-    entry = Entry(window)
-    entry.pack()
-    entry.focus_set()
-    Button(window, text="Okay", command= store(computerID))
+    Label(window, text="Add desktop sale").pack()
+    Label(window, text="ComputerID").pack()
+    entry1 = Entry(window)
+    entry1.pack()
+
+    def storeCompID():
+        userInput = entry1.get()
+        return userInput
+    Button(window, text="Okay", command=storeCompID).pack()
+
+    Label(window, text="Memory").pack()
+    entry2 = Entry(window)
+    entry2.pack()
+
+    def storeMem():
+        userInput = entry2.get()
+        return userInput
+    Button(window, text="Okay", command=storeMem).pack()
+
+    Label(window, text="Storage").pack()
+    entry3 = Entry(window)
+    entry3.pack()
+
+    def storeSto():
+        userInput = entry3.get()
+        return userInput
+    Button(window, text="Okay", command=storeSto).pack()
+
+    Label(window, text="Clock Speed").pack()
+    entry4 = Entry(window)
+    entry4.pack()
+
+    def storeCS():
+        userInput = entry4.get()
+        return userInput
+    Button(window, text="Okay", command=storeCS).pack()
+
+    Label(window, text="Monitor").pack()
+    entry5 = Entry(window)
+    entry5.pack()
+
+    def storeMon():
+        userInput = entry5.get()
+        return userInput
+    Button(window, text="Okay", command=storeMon).pack()
+
+    def buildD():
+        tempCID = storeCompID()
+        tempMe = storeMem()
+        tempS = storeSto()
+        tempCS = storeCS()
+        tempMo = storeMon()
+        temp = obj = desktop(tempCID, tempMe, tempS, tempCS, tempMo)
+        desktops.append(temp)
+        Label(window, text="Sale Added").pack()
+
+    Button(window, text="Add Desktop", command=buildD).pack()
+
+
+def addL():
+    window = Toplevel(top)
+    window.title("Add Laptop sale")
+    window.geometry("200x200")
+    Label(window, text="Add Laptop sale").pack()
+    Label(window, text="ComputerID").pack()
+    entry1 = Entry(window)
+    entry1.pack()
+
+    def storeCompID():
+        userInput = entry1.get()
+        return userInput
+    Button(window, text="Okay", command=storeCompID).pack()
+
+    Label(window, text="Memory").pack()
+    entry2 = Entry(window)
+    entry2.pack()
+
+    def storeMem():
+        userInput = entry2.get()
+        return userInput
+    Button(window, text="Okay", command=storeMem).pack()
+
+    Label(window, text="Storage").pack()
+    entry3 = Entry(window)
+    entry3.pack()
+
+    def storeSto():
+        userInput = entry3.get()
+        return userInput
+    Button(window, text="Okay", command=storeSto).pack()
+
+    Label(window, text="Clock Speed").pack()
+    entry4 = Entry(window)
+    entry4.pack()
+
+    def storeCS():
+        userInput = entry4.get()
+        return userInput
+    Button(window, text="Okay", command=storeCS).pack()
+
+    Label(window, text="Monitor").pack()
+    entry5 = Entry(window)
+    entry5.pack()
+
+    def storeWe():
+        userInput = entry5.get()
+        return userInput
+    Button(window, text="Okay", command=storeWe).pack()
+
+    def buildD():
+        tempCID = storeCompID()
+        tempMe = storeMem()
+        tempS = storeSto()
+        tempCS = storeCS()
+        tempW = storeWe()
+        temp = obj = laptop(tempCID, tempMe, tempS, tempCS, tempW)
+        laptops.append(temp)
+        Label(window, text="Sale Added").pack()
+
+    Button(window, text="Add Laptop", command=buildD).pack()
 
 
 def quit():
-    exit()   
-a = Button(top, text = "View all sales", command = showsales)
-a.pack(padx = 10, pady = 5)
-b = Button(top, text = "Add Desktop Sale", command = addD)
-b.pack(padx = 10, pady = 5)
-c = Button(top, text = "Add Laptop Sale", command = buttonTest)
-c.pack(padx = 10, pady = 5)
-d = Button(top, text = "Quit", command = quit)
-d.pack(padx = 10, pady = 5)
+    exit()
+
+
+a = Button(top, text="View all sales", command=showsales)
+a.pack(padx=10, pady=5)
+b = Button(top, text="Add Desktop Sale", command=addD)
+b.pack(padx=10, pady=5)
+c = Button(top, text="Add Laptop Sale", command=buttonTest)
+c.pack(padx=10, pady=5)
+d = Button(top, text="Quit", command=quit)
+d.pack(padx=10, pady=5)
 top.mainloop()
+
 
 def dIdCheck(computerID):
     bool = False
@@ -102,7 +222,6 @@ def lIdCheck(computerID):
     z = 0
     if(computerID.startswith("L")) and len(computerID) == 4:
         z = 1
-        
 
     for x in laptops:
         if (computerID == laptops[y].computerID):
@@ -112,7 +231,6 @@ def lIdCheck(computerID):
     if(x == 1 and z == 1):
         bool = True
         return bool
-
 
 
 userInput = 0
@@ -189,6 +307,7 @@ while True:
             bool = lIdCheck(tempCID)
             if (bool):
                 break
+
             else:
                 print("Desktop Computer ID must start with 'L'")
                 print("")
